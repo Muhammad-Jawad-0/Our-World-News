93182f6cb6cb0109fb6e27b56ab1295ac0823e14
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-08-31 01:10:52 +0500

    news website using javascript

---
+++ app.js
@@ -0,0 +1,62 @@
+// passwordfornewsapi1122.js
+// passwordfornewsapi1122.js
+
+// myApiKey = 08eadbd9efbe48f6a55976b742e76b74
+
+// https://newsapi.org/v2/everything?q=tesla&from=2024-07-30&sortBy=publishedAt&apiKey=08eadbd9efbe48f6a55976b742e76b74
+
+// fetch("https://newsapi.org/v2/everything?q=pakistan&from=2024-07-30&sortBy=publishedAt&apiKey=08eadbd9efbe48f6a55976b742e76b74")
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+
+let getNews = (search, page) => {
+  let url = `https://newsapi.org/v2/everything?q=${
+    search ? search : "Pakistan"
+  }&page_size=12&from=2024-07-30&sortBy=publishedAt&apiKey=08eadbd9efbe48f6a55976b742e76b74&page=${
+    page ? page : 1
+  }&pageSize=12`;
+
+  let req = new Request(url);
+
+  fetch(req)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      let newsCard = document.getElementById("news");
+      const articles = res.articles;
+      for (let i = 0; i < articles.length; i++) {
+        const { urlToImage, title, description ,url} = articles[i];
+        newsCard.innerHTML += `
+        <div class="card mb-3" style="width: 18rem">
+        <img src="${urlToImage}" class="news-img card-img-top" alt="..." />
+        <div class="card-body">
+        <h5 class="card-title">${title.slice(0, 20)}...</h5>
+        <p class="card-text">
+        ${description.slice(0, 70)}...
+        </p>
+        </div>
+        </div>
+        `;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+getNews();
+
+let page = 1;
+
+const searchNews = () => {
+  let search = document.getElementById("search");
+  let newsCard = document.getElementById("news");
+  newsCard.innerHTML = "";
+  getNews(search.value);
+  search.value = "";
+};
+
+const loadMore = () => {
+  let search = document.getElementById("search");
+  page++;
+  getNews(search.value, page);
+};
