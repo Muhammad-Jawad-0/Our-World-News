28aa6724a08370674c525e03d0126bd36578d6a8
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-08-31 20:50:15 +0500

    news website using javascript practice complete

--- app.js
+++ app.js
@@ -10,12 +10,11 @@
 //     .then(res => console.log(res))
 //     .catch(err => console.log(err))
 
+const content = document.getElementById("content");
+const loader = document.getElementById("loader");
+
 let getNews = (search, page) => {
-  let url = `https://newsapi.org/v2/everything?q=${
-    search ? search : "Pakistan"
-  }&page_size=12&from=2024-07-30&sortBy=publishedAt&apiKey=08eadbd9efbe48f6a55976b742e76b74&page=${
-    page ? page : 1
-  }&pageSize=12`;
+  let url = `https://newsapi.org/v2/everything?q=${search ? search : "Pakistan"}&page_size=12&from=2024-07-30sortBy=publishedAt&apiKey=08eadbd9efbe48f6a55976b742e76b74&page=${page ? page : 1}&pageSize=12`;
 
   let req = new Request(url);
 
@@ -23,10 +22,12 @@ let getNews = (search, page) => {
     .then((res) => res.json())
     .then((res) => {
       console.log(res);
+      loader.style.display = "none"
+      content.style.display = "flex";
       let newsCard = document.getElementById("news");
       const articles = res.articles;
       for (let i = 0; i < articles.length; i++) {
-        const { urlToImage, title, description ,url} = articles[i];
+        const { urlToImage, title, description, publishedAt} = articles[i];
         newsCard.innerHTML += `
         <div class="card mb-3" style="width: 18rem">
         <img src="${urlToImage}" class="news-img card-img-top" alt="..." />
@@ -36,11 +37,12 @@ let getNews = (search, page) => {
         ${description.slice(0, 70)}...
         </p>
         </div>
+        <span class="badge text-bg-info">${moment(publishedAt).fromNow()}</span>
         </div>
         `;
       }
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("err >>>", err));
 };
 
 getNews();
@@ -51,6 +53,8 @@ const searchNews = () => {
   let search = document.getElementById("search");
   let newsCard = document.getElementById("news");
   newsCard.innerHTML = "";
+  loader.style.display = "flex"
+  content.style.display = "none";
   getNews(search.value);
   search.value = "";
 };
@@ -60,3 +64,9 @@ const loadMore = () => {
   page++;
   getNews(search.value, page);
 };
+
+window.onscroll = function(ev){
+  if((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight){
+    loadMore()
+  }
+}
